376f666e3e04d73b76f9e9f978c4b82c0f9030a3
brockmeyer7 <brockmeyer05@gmail.com>
2022-12-11 12:56:38 -0600

    work on register view

---
+++ stock_simulator/helpers.py
@@ -0,0 +1,50 @@
+import os
+# import requests
+from django.shortcuts import render
+import urllib.parse
+
+from functools import wraps
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render("apology.html", {'top': code, 'bottom': escape(message)}), code
+
+
+# def lookup(symbol):
+#     """Look up quote for symbol."""
+
+#     # Contact API
+#     try:
+#         api_key = os.environ.get("API_KEY")
+#         url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+#         response = requests.get(url)
+#         response.raise_for_status()
+#     except requests.RequestException:
+#         return None
+
+#     # Parse response
+#     try:
+#         quote = response.json()
+#         return {
+#             "name": quote["companyName"],
+#             "price": float(quote["latestPrice"]),
+#             "symbol": quote["symbol"]
+#         }
+#     except (KeyError, TypeError, ValueError):
+        # return None
+
+
+# def usd(value):
+#     """Format value as USD."""
+#     return f"${value:,.2f}"
--- stock_simulator/views.py
+++ stock_simulator/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse as response, HttpRequest
+from django.http import HttpResponse as response
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from . helpers import apology
+import django.forms as forms
 
-def index(request):
-    user = ""
-    return render(request, 'register.html', {'user': user})
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+    confirmation = forms.CharField()
+
+
+@require_http_methods(['GET', 'POST'])
+def register(request):
+    if request.method=='POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            confirmation = form.cleaned_data['confirmation']
+
+            if not username:
+                return apology("Input valid username")
+            elif not password or not confirmation:
+                return apology("Input password and confirmation")
+            elif password != confirmation:
+                return apology("Passwords do not match")
+            
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            return response()
+    return render(request, 'register.html')
