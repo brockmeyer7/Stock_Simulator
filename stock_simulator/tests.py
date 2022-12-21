f11f7d5d3d058c3dff006b0110c8e906b279c64d
brockmeyer7 <brockmeyer05@gmail.com>
2022-12-21 06:32:34 -0600

    add tests and update docker-compose.yml

--- stock_simulator/tests.py
+++ stock_simulator/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+import pytest
+from . models import User
+from . helpers import lookup, usd
+from django.urls import reverse
 # Create your tests here.
+@pytest.mark.django_db
+def test_user_creation():
+    User.objects.create_user(username='john', password='1234')
+    assert User.objects.count() == 1
+
+def test_lookup():
+    symbol = 'NFLX'
+    assert lookup(symbol) != None
+
+def test_usd():
+    assert usd(100) == '$100.00'
