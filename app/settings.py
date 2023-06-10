266a253bc21856474cb9a190713464b7e8f523f6
brockmeyer7 <brockmeyer05@gmail.com>
2023-06-10 12:45:19 -0500

    update settings.py

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-078edo)7_in0@^@nowhee^@)3#rf8b_wzz^gcg=&@(=hesr-df'
+SECRET_KEY = environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
